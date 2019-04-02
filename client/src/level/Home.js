32ff4604288262833cd4ee739a8ebb47117fb8fc
quentin basset <quentin.basset@ochelys.com>
2019-04-02 17:01:43 +0200

    :sparkles: add Home Component

---
+++ client/src/level/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <Link to="/level">
+          <Button variant="contained" color="primary">
+            Commencer
+          </Button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Home;
