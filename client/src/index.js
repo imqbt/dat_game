6ad7783fe6668e27c633eb9fa38736639bdb1f9c
quentin basset <quentin.basset@ochelys.com>
2019-04-02 17:06:10 +0200

    :sparkles: routes added

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './level/Home'
+import Level from './level/Level'
+import NotFound from './common/NotFound'
+import { Route, HashRouter as Router, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/level" component={Level} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
