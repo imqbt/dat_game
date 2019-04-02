82f392483208baac7ec9a0c8445ef23f1696077e
quentin basset <quentin.basset@ochelys.com>
2019-04-02 20:12:56 +0200

    :sparkles: add mobx provider

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,21 @@ import Home from './level/Home'
 import Level from './level/Level'
 import NotFound from './common/NotFound'
 import { Route, HashRouter as Router, Switch } from 'react-router-dom'
+import { Provider } from 'mobx-react';
+import {  } from 'mobx-react';
+import TimerStore from  './stores/TimerStore'
 
-const routing = (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/level" component={Level} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+const providerAndRouting = (
+  <Provider TimerStore = {TimerStore} >
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/level" component={Level} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </Provider>
 )
-ReactDOM.render(routing, document.getElementById('root'))
+ReactDOM.render(providerAndRouting, document.getElementById('root'))
 
 serviceWorker.unregister();
