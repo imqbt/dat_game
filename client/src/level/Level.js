9021434a3d4eb585a0cc08676bf9a395c006f765
quentin basset <quentin.basset@ochelys.com>
2019-04-02 17:02:35 +0200

    :sparkles: add Level component

---
+++ client/src/level/Level.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button'
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+ 
+const code = `function add() {
+  console.log("asv")
+}
+`;
+
+class Level extends Component {
+  state = { code };
+
+  render() {
+    return (
+      <div className="Level">
+        <Editor
+          value={this.state.code}
+          onValueChange={code =>  this.setState({ code })}
+          highlight={code => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+        />
+        <Button variant="contained" color="primary" onClick={() => this.execute()}>
+          Tester
+        </Button>
+      </div>
+    );
+  }
+
+  execute = () => {
+    // eslint-disable-next-line
+    eval(this.state.code + ' add()');
+  }
+}
+
+export default Level;
