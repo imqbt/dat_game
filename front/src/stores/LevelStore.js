9098a76fb16f329ed9f057859e8082860354f37e
quentin basset <quentin.basset@ochelys.com>
2019-04-04 14:57:21 +0200

    :sparkles: add tests to LevelStore

--- front/src/stores/LevelStore.js
+++ front/src/stores/LevelStore.js
@@ -4,7 +4,9 @@ import {
   action,
   decorate,
 } from 'mobx'
+import TimerStore from './TimerStore'
 
+console.log(TimerStore)
 class LevelStore {
   currentLevel = {}
 
@@ -12,17 +14,34 @@ class LevelStore {
 
   levels = []
 
+  currentTest = {}
+
+  currentTestId = 0
+
+  test = []
+
   loadLevels = async () => {
     const response = await fetch('/levels')
     const levels = await response.json()
     runInAction(() => {
       this.levels = levels
       this.currentLevel = this.levels[this.currentLevelId]
+      this.loadTests(this.currentLevel.uuid)
+    })
+  }
+
+  loadTests = async uuid => {
+    const response = await fetch(`/levels/${uuid}/tests`)
+    const tests = await response.json()
+    runInAction(() => {
+      this.tests = tests
+      this.currentTest = this.tests[this.currentTestId]
     })
   }
 
   nextLevel = () => {
     this.currentLevelId = this.currentLevelId + 1
+    TimerStore.saveTime(this.currentLevelId)
     this.currentLevel =
       this.currentLevelId >= this.levels.length
         ? false
@@ -34,6 +53,9 @@ decorate(LevelStore, {
   currentLevel: observable,
   levels: observable,
   nextLevel: action,
+  currentTest: observable,
+  tests: observable,
+  nextTest: action,
 })
 
 export default new LevelStore()
