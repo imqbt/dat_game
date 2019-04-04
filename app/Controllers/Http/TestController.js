8d89d8cb09b15793066007f906822d064c9f9f59
quentin basset <quentin.basset@ochelys.com>
2019-04-04 13:37:40 +0200

    :wrench: fix TestController for nested ressources

--- app/Controllers/Http/TestController.js
+++ app/Controllers/Http/TestController.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const Test = use('App/Models/Test')
+const Level = use('App/Models/Level')
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
 
 /**
  * Resourceful controller for interacting with tests
@@ -17,33 +17,10 @@ class TestController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-    return Test.all()
-  }
-
-  /**
-   * Render a form to be used for creating a new test.
-   * GET tests/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new test.
-   * POST tests
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
+  async index ({ request, response }) {
+    const level = await Level.findByOrFail('uuid', request.params.levels_id)
+    return level.tests().fetch()
   }
 
   /**
@@ -53,44 +30,9 @@ class TestController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params, request, response }) {
     return Test.findOrFail(params.id)
-  }
-
-  /**
-   * Render a form to update an existing test.
-   * GET tests/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update test details.
-   * PUT or PATCH tests/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a test with id.
-   * DELETE tests/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
   }
 }
 
