887f2e0b06ca9a0c2af8bb091e2ce529e1ae0a1b
quentin basset <quentin.basset@ochelys.com>
2019-04-06 22:30:59 +0200

    :sparkles: install validator

--- start/app.js
+++ start/app.js
@@ -15,6 +15,7 @@ const providers = [
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
+  '@adonisjs/validator/providers/ValidatorProvider',
   '@adonisjs/lucid/providers/LucidProvider'
 ]
 
