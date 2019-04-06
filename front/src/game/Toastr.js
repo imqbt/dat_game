3ca995a17594a55d7706140ee82aa8c3a8a87ce1
quentin basset <quentin.basset@ochelys.com>
2019-04-06 10:52:16 +0200

    :sparkles: changed Toastr to functional Component

--- front/src/game/Toastr.js
+++ front/src/game/Toastr.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
 import { observer, inject } from 'mobx-react'
 
-class Toastr extends Component {
-  render() {
-    return (
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={this.props.LevelStore.showNextLevelAlert}
-        autoHideDuration={3000}
-        message={
-          <span id="message-id">Bravo, vous passez au niveau suivant</span>
-        }
-      />
-    )
-  }
-}
+const Toastr = inject('LevelStore')(observer(({ LevelStore }) => {
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={LevelStore.showNextLevelAlert}
+      autoHideDuration={3000}
+      message={
+        <span id="message-id">Bravo, vous passez au niveau suivant</span>
+      }
+    />
+  )
+}))
 
-export default inject('LevelStore')(observer(Toastr))
+export default Toastr
+
