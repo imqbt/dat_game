f4d717bb3c2b5c45343fbf1177c5775162dff3c5
quentin basset <quentin.basset@ochelys.com>
2019-04-06 10:48:40 +0200

    :sparkles: change Timer to functional Component

--- front/src/game/Timer.js
+++ front/src/game/Timer.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { observer, inject } from 'mobx-react'
 import TextField from '@material-ui/core/TextField'
 import Clok from './Clok'
 
-class Timer extends Component {
-  render() {
-    return (
-      <TextField
-        disabled
-        id="outlined-disabled"
-        label="Temps passé"
-        value={Clok({ time: this.props.TimerStore.time })}
-        margin="normal"
-        variant="outlined"
-      />
-    )
-  }
-}
+const Timer = inject('TimerStore')(observer(({ TimerStore }) => {
+  return (
+    <TextField
+      disabled
+      id="outlined-disabled"
+      label="Temps passé"
+      value={Clok({ time: TimerStore.time })}
+      margin="normal"
+      variant="outlined"
+    />
+  )
+}))
 
-export default inject('TimerStore')(observer(Timer))
+export default Timer
+
